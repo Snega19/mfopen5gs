28d6b3a5d63ca64a7d2fabe89657c1186623921c
Snega <117353362+Snega19@users.noreply.github.com>
2023-10-12 18:36:41 +0530

    Update monitoring.tf

--- monitoring/monitoring.tf
+++ monitoring/monitoring.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         	   = "monitoring-bucket"
+    bucket         	   = "monitoring-bucket12"
     key              	   = "state/terraform.tfstate1"
     region         	   = "us-east-1"
     encrypt        	   = true
