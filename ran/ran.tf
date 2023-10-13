35dacdb9dba429ed586a48c5c806797640938a8f
Snega <117353362+Snega19@users.noreply.github.com>
2023-10-13 11:15:51 +0530

    Update ran.tf

--- ran/ran.tf
+++ ran/ran.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         	   = "ran-bucket"
+    bucket         	   = "ran-bucket-mfopen5g"
     key              	   = "state/terraform.tfstate1"
     region         	   = "us-east-1"
     encrypt        	   = true
